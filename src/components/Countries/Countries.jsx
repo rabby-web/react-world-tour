568bf5a93c965868b0e5421284dc36346e526ef9
rabby-web <rabby.webdeveloper@gmail.com>
2023-09-09 22:56:05 +0600

    country card UI

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] =useState([]);
@@ -10,10 +11,12 @@ const Countries = () => {
     },[])
     return (
         <div>
-            <h3>Countries: {countries.length}</h3>
+            <h3>React Countries Tour</h3>
+            <div className="country-container">
             {
                 countries.map(country =><Country key={country.cca3} country={country}></Country>)
             }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,17 @@
 import './Country.css'
 
 const Country = ({country}) => {
-    const {name,flags} = country;
+    const {name,flags,population,area} = country;
     console.log(country)
     return (
         <div className='country'>
-            <img src={flags.png} alt="" />
+            <div className='w-center'>
+            <img className='img-height' src={flags.png} alt="" />
+            </div>
+            <hr />
             <h3>Name: {name?.common}</h3>
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
         </div>
     );
 };
