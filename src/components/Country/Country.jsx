e0f0f880e25878eb7acabf719b20f34204a1e2bf
rabby-web <rabby.webdeveloper@gmail.com>
2023-09-09 21:12:56 +0600

    country data map and useState and useEffect use

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,13 @@
+import './Country.css'
+
+const Country = ({country}) => {
+    const {name} = country;
+    console.log(country)
+    return (
+        <div className='country'>
+            <h3>Name: {name?.common}</h3>
+        </div>
+    );
+};
+
+export default Country;
